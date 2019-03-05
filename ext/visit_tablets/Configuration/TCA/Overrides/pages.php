c0975de8cc560715a07e795eebe4ba900896f0fd
Kris <krispin.raich@fh-kufstein.ac.at>
2019-03-05 14:02:36 +0100

    removed extensions

--- ext/visit_tablets/Configuration/TCA/Overrides/pages.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-// BackendLayouts
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'visit_tablets',
-    'Configuration/WebLayout/BackendLayouts.txt',
-    'Visit App: Backend Layouts'
-);
