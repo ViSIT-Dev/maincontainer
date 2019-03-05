77f4118179425ebba593660377b1838c9347230d
Kris <krispin.raich@fh-kufstein.ac.at>
2019-03-05 15:50:58 +0100

    moved git init to run

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ RUN	apt-get update && \
 	apt-get install -y mysql-server && \
 	/etc/init.d/mysql start && \
 	mysql -u root < /tmp/db.sql && \
-	git clone https://github.com/ViSIT-Dev/appbundle.git /var/www/html/typo3conf/ext/visit_tablets &&\
 	chown www-data:www-data /var/www/html -hR && \
 	rm -f /tmp/db.sql && \
 	apt-get clean
--- res/run.sh
+++ res/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ ! -d /var/www/html/typo3conf/ext/visit_tablets ]
+then
+    git clone https://github.com/ViSIT-Dev/appbundle.git /var/www/html/typo3conf/ext/visit_tablets;
+    chown www-data:www-data /var/www/html -hR && \
+fi
+
+
 /etc/init.d/apache2 start > /dev/null
 /etc/init.d/mysql start > /dev/null
 touch /var/log/mysql/error.log /var/log/apache2/error.log
