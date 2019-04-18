46f9a7c749f96f84a1d924a6bf0fb1a0b1c1c043
Robert Kathrein <office@kara-software.at>
2019-04-18 17:19:24 +0200

    added cron and better debug with RUN

--- res/run.sh
+++ res/run.sh
@@ -11,9 +11,10 @@ chown www-data:www-data /var/www -hR
 touch /var/log/mysql/error.log /var/log/apache2/error.log /var/log/cron.log
 /etc/init.d/apache2 start > /dev/null
 wget -qO- http://localhost/typo3/ &> /dev/null 
+/etc/init.d/cron start > /dev/null
 
 su www-data -s /bin/bash -c "env HOME=/var/www/syncthing && nohup /usr/bin/syncthing -home /var/www/syncthing -gui-address 0.0.0.0:8384 &>/var/www/syncthing.log &"
 
-cron &
+
 
 tail -f /var/log/mysql/error.log /var/log/apache2/error.log /var/www/syncthing.log /var/log/cron.log
